9659219b0f4c2fe76e1b198d117343df0f2227ca
Marcos Soares <marcossoares100@gmail.com>
2021-02-14 11:00:53 +0000

    created method to cleanup string

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"log"
+	"regexp"
+)
+
 func ReverseIntSlice(s []int) []int {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
@@ -18,4 +23,14 @@ func ReplaceAtIndex(in string, r rune, i int) string {
 	out := []rune(in)
 	out[i] = r
 	return string(out)
+}
+
+func CleanString(s string) string {
+	// Make a Regex to say we only want letters and numbers
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+	processedString := reg.ReplaceAllString(s, "")
+	return processedString
 }
